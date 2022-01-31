84ccd98e0c445259dadeb2652e4d60bde485bec4
Jhoglas <jhoglassx@gmail.com>
2022-01-31 15:18:34 -0300

    corrections in view

--- app/src/main/java/com/ciandt/breweryees/MainActivity.kt
+++ app/src/main/java/com/ciandt/breweryees/MainActivity.kt
@@ -1,26 +1,40 @@
 package com.ciandt.breweryees
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.ciandt.breweryees.Model.cervejaria
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.ciandt.breweryees.Model.fakeCervejaria
 import com.ciandt.breweryees.databinding.ActivityMainBinding
+import com.kingfisher.easyviewindicator.AnyViewIndicator
+import com.kingfisher.easyviewindicator.RecyclerViewIndicator
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    var horizontalIndicator: RecyclerViewIndicator? = null
     override fun onCreate(savedInstanceState: Bundle?) {
 
         Thread.sleep(2000)
         setTheme(R.style.Theme_Breweryees)
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater, null, false)
+        binding = com.ciandt.breweryees.databinding.ActivityMainBinding.inflate(layoutInflater, null, false)
 
         setContentView(binding.root)
 
-        topRecyclerView.adapter = CervejariaAdapter(fakeCervejaria())
-        topRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        setupLinearRecyclerViewHorizontal()
     }
+
+    private fun setupLinearRecyclerViewHorizontal() {
+        topRecyclerView.layoutManager = LinearLayoutManager(this,RecyclerView.HORIZONTAL,false)
+        topRecyclerView.adapter = CervejariaAdapter(fakeCervejaria())
+
+        topRecyclerView.setLayoutManager(topRecyclerView.layoutManager)
+
+        horizontalIndicator!!.setRecyclerView(topRecyclerView)
+    }
+
 }
