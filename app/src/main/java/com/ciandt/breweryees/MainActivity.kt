c661f2c344ecf46824831ced9833766e94766d19
Jhoglas <jhoglassx@gmail.com>
2022-01-31 16:58:44 -0300

    add indicator in RecyclerView

--- app/src/main/java/com/ciandt/breweryees/MainActivity.kt
+++ app/src/main/java/com/ciandt/breweryees/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.ciandt.breweryees
 
+import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.OnScrollListener
+
 import com.ciandt.breweryees.Model.fakeCervejaria
 import com.ciandt.breweryees.databinding.ActivityMainBinding
-import com.kingfisher.easyviewindicator.AnyViewIndicator
+
 import com.kingfisher.easyviewindicator.RecyclerViewIndicator
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     var horizontalIndicator: RecyclerViewIndicator? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         Thread.sleep(2000)
-        setTheme(R.style.Theme_Breweryees)
+        //setTheme(R.style.Theme_Breweryees)
         super.onCreate(savedInstanceState)
 
-        binding = com.ciandt.breweryees.databinding.ActivityMainBinding.inflate(layoutInflater, null, false)
+        binding = ActivityMainBinding.inflate(layoutInflater, null, false)
 
         setContentView(binding.root)
 
@@ -29,12 +30,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupLinearRecyclerViewHorizontal() {
-        topRecyclerView.layoutManager = LinearLayoutManager(this,RecyclerView.HORIZONTAL,false)
+        topRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL,false)
         topRecyclerView.adapter = CervejariaAdapter(fakeCervejaria())
-
-        topRecyclerView.setLayoutManager(topRecyclerView.layoutManager)
-
-        horizontalIndicator!!.setRecyclerView(topRecyclerView)
+        recyclerViewIndicator.setRecyclerView(topRecyclerView);
     }
-
 }
