293ac9fb54f6d9f243eb0fb8040577c4ef2e56e7
CIANDT\rodrigovidal <rodrigovidal@ciandt.com>
2022-02-08 10:08:05 -0300

    View details finished.

--- app/src/main/java/com/ciandt/breweryees/ui/DetailsActivity.kt
+++ app/src/main/java/com/ciandt/breweryees/ui/DetailsActivity.kt
@@ -1,11 +1,12 @@
-package com.ciandt.breweryees
+package com.ciandt.breweryees.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ciandt.breweryees.R
 
-class MainActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details)
     }
 }
