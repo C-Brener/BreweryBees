a0f510b3fac29d679ab055e120cf6a776ec8ae29
Jhoglas <jhoglassx@gmail.com>
2022-02-17 15:28:35 -0300

    implemented retrofit

--- app/src/main/java/com/ciandt/breweryees/api/BreweriesService.kt
+++ app/src/main/java/com/ciandt/breweryees/api/BreweriesService.kt
@@ -1,4 +1,10 @@
 package com.ciandt.breweryees.api
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface BreweriesService {
+
+    @GET("/breweries")
+    suspend fun breweriesCity(@Query(value = "by_city",encoded = true) by_city : String)
 }
