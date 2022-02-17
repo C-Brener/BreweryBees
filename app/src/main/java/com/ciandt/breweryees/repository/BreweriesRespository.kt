8a348563906f883aafb93f64a7dbdd9164d10c74
Jhoglas <jhoglassx@gmail.com>
2022-02-17 15:24:30 -0300

    implemented retrofit

--- app/src/main/java/com/ciandt/breweryees/repository/BreweriesRespository.kt
+++ app/src/main/java/com/ciandt/breweryees/repository/BreweriesRespository.kt
@@ -1,11 +1,26 @@
 package com.ciandt.breweryees.repository
 
+import com.ciandt.breweryees.Model.BreweriesModel
 import com.ciandt.breweryees.api.BreweriesService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class BreweriesRespository {
     val service : BreweriesService
 
-    init {
+    companion object{
+        private const val BASE_URL = "https://bootcamp-mobile-01.eastus.cloudapp.azure.com"
+    }
 
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        service = retrofit.create(BreweriesService::class.java)
+    }
+
+    suspend fun getBreweriesTopTen() : List<BreweriesModel>{
+        return service.breweriesTopTen()
     }
 }
