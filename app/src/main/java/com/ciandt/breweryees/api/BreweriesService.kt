6652103c12a09cc70cd5272440a42fb456de2e1f
Jhoglas <jhoglassx@gmail.com>
2022-02-24 10:38:35 -0300

    create view model

--- app/src/main/java/com/ciandt/breweryees/api/BreweriesService.kt
+++ app/src/main/java/com/ciandt/breweryees/api/BreweriesService.kt
@@ -3,16 +3,18 @@ package com.ciandt.breweryees.api
 import com.ciandt.breweryees.Model.BreweriesModel
 import com.ciandt.breweryees.Model.BreweriesRatingModel
 import com.skydoves.sandwich.ApiResponse
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface BreweriesService {
 
     @GET("/breweries/topTen")
     suspend fun breweriesTopTen():ApiResponse<List<BreweriesModel>>
 
+    @Headers(
+        "Content-Type: application/json",
+        "Accept: application/json")
     @POST("/breweries")
-    suspend fun setBreweriesRating():ApiResponse<BreweriesRatingModel>
+    suspend fun setBreweriesRating(@Body breweriesRatingModel: BreweriesRatingModel):ApiResponse<BreweriesRatingModel>
+
 
 }
--- app/src/main/java/com/ciandt/breweryees/ui/DetailsActivity.kt
+++ app/src/main/java/com/ciandt/breweryees/ui/DetailsActivity.kt
@@ -1,13 +1,47 @@
 package com.ciandt.breweryees.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.lifecycle.ViewModel
 import com.ciandt.breweryees.R
+import com.ciandt.breweryees.databinding.ActivityDetailsBinding
+import com.ciandt.breweryees.databinding.FragmentModalBinding
+import com.ciandt.breweryees.ui.details.DetailsViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetailsBinding
+    private val viewModel : DetailsViewModel by viewModel()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+
+        binding.btnRating.setOnClickListener{
+
+        }
+
     }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+
+    }
+
+    private fun showRatingDialog(){
+        val dialog = dialog()
+        val sheetBind: FragmentModalBinding = FragmentModalBinding.inflate(layoutInflater)
+    }
+
+
+
+
+
 }
